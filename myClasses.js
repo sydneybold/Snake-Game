ce95580fb763e312405988327e80d26be8624045
Sydney Bold <sydneybold@Sydneys-MacBook-Pro-2.local>
2020-12-18 23:25:25 -0800

    Final Commit

--- myClasses.js
+++ myClasses.js
@@ -31,115 +31,145 @@ class Snake {
     this._dy = newDY;
   }
 
-  drawSnakePart(snakePart) {
-    context.fillStyle = "#1d18b5";
-    context.fillRect(snakePart.x, snakePart.y, 40, 40);
+  hasGameStarted() {
+    /*
+      Purpose: Check if the game has started.
+      Inputs: None.
+      Returns: True if the game has started and false if the game hasn't started.
+    */
+
+    // The game hasn't started if the snake is not moving and the game hasn't ended
+    if ((snake.dx == 0) && (snake.dy == 0) && (snake.hasGameEnded() == false)) {
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
-  drawSnakeEyes() {
-    if ((snake.dx == 40) || ((snake.dy == 0) && (snake.dx == 0))) {
-      context.beginPath();
-      context.arc(snake.head.x + 18, snake.head.y + 8, 8, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 18, snake.head.y + 32, 8, 0, 2 * Math.PI);
-      context.fillStyle = "white";
-      context.fill();
+  startGame(event) {
+    /*
+      Purpose: To start the game once the user has pressed the space bar.
+      Inputs: The event.
+      Returns: None.
+    */
+    var keyPressed = event.keyCode;
 
-      context.beginPath();
-      context.arc(snake.head.x + 22, snake.head.y + 8, 3, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 22, snake.head.y + 32, 3, 0, 2 * Math.PI);
-      context.fillStyle = "black";
-      context.fill();
-    }
-
-    if (snake.dx == -40) {
-      context.beginPath();
-      context.arc(snake.head.x + 18, snake.head.y + 8, 8, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 18, snake.head.y + 32, 8, 0, 2 * Math.PI);
-      context.fillStyle = "white";
-      context.fill();
-
-      context.beginPath();
-      context.arc(snake.head.x + 14, snake.head.y + 8, 3, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 14, snake.head.y + 32, 3, 0, 2 * Math.PI);
-      context.fillStyle = "black";
-      context.fill();
-    }
-
-    if (snake.dy == 40) {
-      context.beginPath();
-      context.arc(snake.head.x + 8, snake.head.y + 14, 8, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 32, snake.head.y + 14, 8, 0, 2 * Math.PI);
-      context.fillStyle = "white";
-      context.fill();
-
-      context.beginPath();
-      context.arc(snake.head.x + 8, snake.head.y + 18, 3, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 32, snake.head.y + 18, 3, 0, 2 * Math.PI);
-      context.fillStyle = "black";
-      context.fill();
-    }
-
-    if (snake.dy == -40) {
-      context.beginPath();
-      context.arc(snake.head.x + 8, snake.head.y + 22, 8, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 32, snake.head.y + 22, 8, 0, 2 * Math.PI);
-      context.fillStyle = "white";
-      context.fill();
-
-      context.beginPath();
-      context.arc(snake.head.x + 8, snake.head.y + 18, 3, 0, 2 * Math.PI);
-      context.arc(snake.head.x + 32, snake.head.y + 18, 3, 0, 2 * Math.PI);
-      context.fillStyle = "black";
-      context.fill();
+    if (keyPressed == 32) {     // 32 is the keycode for the space bar
+      snake.dx = 40;            // Starts the motion of the snake
     }
   }
 
   drawSnake() {
+    /*
+      Purpose: To draw the snake.
+      Inputs: None.
+      Returns: None.
+    */
     this.snake.forEach(snake.drawSnakePart);
-    snake.drawSnakeEyes();
+    snake.drawSnakeEyes(snake.head.x, snake.head.y);
+  }
+
+  drawSnakePart(snakePart) {
+    /*
+      Purpose: To draw each individual box of the snake.
+      Inputs: The cordinates of the individual snake part.
+      Returns: None.
+    */
+    context.fillStyle = "#1d18b5";
+    context.fillRect(snakePart.x, snakePart.y, 40, 40);
+  }
+
+  drawSnakeEyes(x, y) {
+    /*
+      Purpose: To draw the eyes of the snake depending on the direction.
+      Inputs: The x and y cordinates of the snake head.
+      Returns: None.
+    */
+
+    // Created an array of each shift from the head of the snake depending on where the eyes are facing
+    // positions = [left white eye x, right white eye x, left white eye y, right white eye y, left black eye x, right black eye x, left black eye y, right black eye y,]
+    if ((snake.dx == 40) || (snake.hasGameStarted() == false) || (snake.hasGameEnded() == true)) {     // Right looking eyes
+      var positions = [18, 18, 8, 32, 22, 22, 8, 32];
+    }
+    else if (snake.dx == -40) {    // Left looking eyes
+      var positions = [18, 18, 8, 32, 14, 14, 8, 32];
+    }
+    else if (snake.dy == 40) {     // Down looking eyes
+      var positions = [8, 32, 14, 14, 8, 32, 18, 18];
+    }
+    else if (snake.dy == -40) {      // Up looking eyes
+      var positions = [8, 32, 22, 22, 8, 32, 18, 18];
+    }
+
+    // Draw the white part of the eyes
+    context.beginPath();
+    context.arc(x + positions[0], y + positions[2], 8, 0, 2 * Math.PI);
+    context.arc(x + positions[1], y + positions[3], 8, 0, 2 * Math.PI);
+    context.fillStyle = "white";
+    context.fill();
+
+    // Draw the pupils of the eyes
+    context.beginPath();
+    context.arc(x + positions[4], y + positions[6], 3, 0, 2 * Math.PI);
+    context.arc(x + positions[5], y + positions[7], 3, 0, 2 * Math.PI);
+    context.fillStyle = "black";
+    context.fill();
   }
 
   moveSnake() {
-    let newHead = {x: snake.head.x + snake.dx, y: snake.head.y + snake.dy};
-    this.snake.unshift(newHead);  // add the head to the front of the list
+    /*
+      Purpose: To move the snake.
+      Inputs: None.
+      Returns: None.
+    */
+    let newHead = {x: snake.head.x + snake.dx, y: snake.head.y + snake.dy};   // Add the x and y movement to the head of the snake
+    this.snake.unshift(newHead);  // Add the new head to the front of the list
+    // If the snake eats the apple, the snake grows and does not remove the last item
     if (apple.checkAppleCollision(snake) == false) {
-      this.snake.pop();          // removes the last item of the list
+      this.snake.pop();          // Removes the last item of the list
     }
   }
 
   changeDirection(event) {
+    /*
+      Purpose: To change the direction of the snake.
+      Inputs: The event.
+      Returns: None.
+    */
     var keyPressed = event.keyCode;
 
-    if (keyPressed == 37 && snake.dx == 0) {
-      console.log("left");
+    if (keyPressed == 37 && snake.dx == 0) {      // Left arrow key
       snake.dx = -40;
       snake.dy = 0;
     }
 
-    if (keyPressed == 39 && snake.dx == 0) {
-      console.log("right");
+    if (keyPressed == 39 && snake.dx == 0) {      // Right arrow key
       snake.dx = 40;
       snake.dy = 0;
     }
 
-    if (keyPressed == 38 && snake.dy == 0) {
-      console.log("up");
+    if (keyPressed == 38 && snake.dy == 0) {      // Up arrow key
       snake.dx = 0;
       snake.dy = -40;
     }
 
-    if (keyPressed == 40 && snake.dy == 0) {
-      console.log("down");
+    if (keyPressed == 40 && snake.dy == 0) {      // Down arrow key
       snake.dx = 0;
       snake.dy = 40;
     }
   }
 
   hasGameEnded() {
+    /*
+      Purpose: To check if the game has ended.
+      Inputs: None.
+      Returns: True if the game has ended and false if the game hasn't ended.
+    */
     let tailCollision = false;
     let boundaryCollision = false;
     // check for collision with the boundaries
-    if ((snake.head.x >= canvas.width) || (snake.head.x < 0) || (snake.head.y >= canvas.width) || (snake.head.y < 0)) {
+    if ((snake.head.x >= gameBoardWidth + xShift) || (snake.head.x < xShift) || (snake.head.y >= gameBoardHeight + yShift) || (snake.head.y < yShift)) {
       boundaryCollision = true;
     }
     // check for collision with snake tail
@@ -156,16 +186,8 @@ class Snake {
       return false;
     }
   }
-
-  hasGameStarted() {
-    if ((snake.dx == 0) && (snake.dy == 0) && snake.hasGameEnded() == false) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
 }
+
 
 class Apple {
 
@@ -184,63 +206,82 @@ class Apple {
   }
 
   drawApple() {
+    /*
+      Purpose: To draw the apple.
+      Inputs: None.
+      Returns: None.
+    */
     context.beginPath();
     context.arc(this._x + 20, this._y + 20, 15, 0, 2 * Math.PI);
     context.fillStyle = "red";
     context.fill();
   }
 
-  moveApple() {
-    var newX = (Math.round(Math.random() * 15)) * 40;
-    var newY = (Math.round(Math.random() * 15)) * 40;
-    apple.x = newX;
-    apple.y = newY;
-  }
-
-  checkAppleCollision(snake) {
+  checkAppleCollision() {
+    /*
+      Purpose: To check if the snake ate the apple.
+      Inputs: None.
+      Returns: True if the snake ate the apple and false if the snake didn't eat the apple.
+    */
     if ((this._x == snake.head.x) && (this._y == snake.head.y)) {
-      console.log("EAT APPLE")
       return true;
     }
     else {
       return false;
     }
   }
+
+  moveApple() {
+    /*
+      Purpose: To move the apple to a new location.
+      Inputs: None.
+      Returns: None.
+    */
+    let validApple = false;               // Assumed new apple location is invalid
+    while (validApple == false) {
+      // Create a new x and y location for the apple
+      var newX = (Math.round(Math.random() * 14)) * 40 + xShift;
+      var newY = (Math.round(Math.random() * 14)) * 40 + yShift;
+      var collisions = 0;     // Set the collisions between the apple and snake to 0
+      for (var i = 1; i < snake.list.length; i++) {   // For each square in the snake...
+        if ((newX == snake.list[i].x) && (newY == snake.list[i].y)) {
+          collisions += 1;    // If the apple collides with the snake square, add one to the collisions
+        }
+      }
+      if (collisions == 0) {    // If the apple never collides with a part of the snake, it is a valid apple location
+        validApple = true;
+        apple.x = newX;
+        apple.y = newY;
+      }
+    }
+  }
 }
 
-function beforeGame() {
-  context.fillStyle = "#14a5de";
-  context.fillRect(160, 80, 280, 360);
-  context.strokeStyle = "black";
-  context.stroke();
 
-  context.fillStyle = "#1d18b5";
-  context.fillRect(180, 360, 240, 60);
+function setUpContext() {
+  /*
+    Purpose: To set up the context.
+    Inputs: None.
+    Returns: The context.
+  */
+  // Get the canvas
+  canvas = document.getElementById("gameCanvas");
+  canvas.style.border = "1px solid black";
 
-  context.font = "40px Comic Sans MS";
-  context.textAlign = "center";
-  context.fillStyle = "white";
-  context.fillText("Start", 300, 400)
-
-  drawCheckeredBackground(280, 100, 160, 240);
+  // Set up the context for the animation
+  context = canvas.getContext("2d");
+  return context;
 }
 
-
-function drawCheckeredBackground(width, height, x, y) {
-  if ((width == canvas.width) && (height == canvas.height)) {
-    var rows = 15;
-    var columns = 15;
-  }
-  else {
-    var rows = 5;
-    var columns = 14;
-  }
-
-  var rowHeight = height / rows;
-  var columnWidth = width / columns;
-
-  console.log("Row Height:", rowHeight);
-  console.log("Column Width:", columnWidth);
+function drawCheckeredBackground(x, y, width, height, size) {
+  /*
+    Purpose: To draw the checkerboard background.
+    Inputs: The x and y cordinates of the top left corner, the width, the height,
+    and the size of each square .
+    Returns: None.
+  */
+  var rows = height / size;
+  var columns = width / size;
 
   for (r = 0; r < rows; r++) {
     for (c = 0; c < columns; c++) {
@@ -250,17 +291,119 @@ function drawCheckeredBackground(width, height, x, y) {
       else {
         context.fillStyle = "#a4db17";
       }
-      context.fillRect((c * columnWidth) + x, (r * rowHeight) + y, rowHeight, columnWidth);
+      context.fillRect((c * size) + x, (r * size) + y, size, size);
     }
   }
 }
 
-function setUpContext() {
-  // Get the canvas
-  canvas = document.getElementById("gameCanvas");
-  canvas.style.border = "1px solid black";
+function drawScore() {
+  /*
+    Purpose: To draw the score.
+    Inputs: None.
+    Returns: None.
+  */
+  context.beginPath();
+  context.arc(75, 75 / 2, 25, 0, 2 * Math.PI);
+  context.fillStyle = "red";
+  context.fill();
 
-  // Set up the context for the animation
-  context = canvas.getContext("2d");
-  return context;
+  context.font = "45px Comic Sans MS";
+  context.textAlign = "center";
+  context.fillStyle = "white";
+  context.fillText(points, 135, 55)
+}
+
+function beforeGame() {
+  /*
+    Purpose: To draw the before game screen.
+    Inputs: None.
+    Returns: None.
+  */
+  commonElementsOfStartAndFinishScreens(0, "-- press space to start game --")
+
+  // Draw the game name
+  context.font = "45px Comic Sans MS";
+  context.textAlign = "center";
+  context.fillStyle = "white";
+  context.fillText("Snake Game", 300 + xShift, 180 + yShift);
+
+  // Set up event listener for when user presses a key down.
+  document.addEventListener("keydown", snake.startGame);
+}
+
+function commonElementsOfStartAndFinishScreens(y_Shift, message) {
+  /*
+    Purpose: To draw the common elements of the before and after game screens
+    Inputs: The y shift of the screes because some elements are shifted down depending
+    on whether it is the begining or ending screen and the message for the user on
+    the bottom of the screen.
+    Returns: None.
+  */
+
+  // draw light blue rectangle
+  context.beginPath();
+  context.rect(159 + xShift, 120 + yShift, 282, 360);
+  context.fillStyle = "#14a5de";
+  context.fill();
+  context.lineWidth = 2;
+  context.strokeStyle = "black";
+  context.stroke();
+
+  // draw snake icon
+  context.fillStyle = "#1d18b5";
+  context.fillRect(180 + xShift, 240 + yShift + y_Shift, 120, 40);
+
+  // draw snake icon's eyes
+  snake.drawSnakeEyes(260 + xShift, 240 + yShift + y_Shift);
+
+  // draw apple icon
+  context.beginPath();
+  context.arc(400 + xShift, 255 + yShift + y_Shift, 15, 0, 2 * Math.PI);
+  context.fillStyle = "red";
+  context.fill();
+
+  // draw dark blue rectangle in bottom of larger light blue rectangle
+  context.fillStyle = "#1d18b5";
+  context.fillRect(170 + xShift, 390 + yShift + y_Shift, 260, 80 - y_Shift);
+
+  // draw text
+  context.font = "20px Comic Sans MS";
+  context.textAlign = "center";
+  context.fillStyle = "white";
+  context.fillText(message, 300 + xShift, 440 + yShift, 260);
+
+  // draw checkerboard strip
+  drawCheckeredBackground(160 + xShift, 280 + yShift + y_Shift, 280, 100, 20);
+}
+
+function afterGame() {
+  /*
+    Purpose: To draw the end game screen.
+    Inputs: None.
+    Returns: None.
+  */
+  commonElementsOfStartAndFinishScreens(20, "-- press space to play again --")
+
+  context.font = "50px Comic Sans MS";
+  context.textAlign = "center";
+  context.fillStyle = "white";
+  context.fillText("Game Over", 300 + xShift, 175 + yShift);
+  context.fillStyle = "#1d18b5";
+  context.fillText("Score:", 275 + xShift, 240 + yShift);
+  context.fillText(points, 375 + xShift, 240 + yShift);
+
+  document.addEventListener("keydown", reloadPage);
+}
+
+function reloadPage(event) {
+  /*
+    Purpose: To reload the page so the user can play the game again.
+    Inputs: The event.
+    Returns: None.
+  */
+  var keyPressed = event.keyCode;
+
+  if (keyPressed == 32) {
+    window.location.reload();        // Reloads the page so the user can play again
+  }
 }
--- newGame.js
+++ newGame.js
@@ -1,22 +1,33 @@
 function main() {
-  frames += 1
+  // Add one to the frames
+  frames += 1;
 
+  // Clear canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawCheckeredBackground(canvas.width, canvas.height, 0, 0);
+
+  // Draw green rectangle for background
+  context.fillStyle = "#45B053";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawCheckeredBackground(xShift, yShift, gameBoardWidth, gameBoardHeight, 40);   // Draws the checkerboard background on board
+  drawScore();                // Displays the user's score
 
   if (snake.hasGameStarted() == false) {
-    beforeGame();
+    beforeGame();             // Screen before the game that allows the user to start the game
+  }
+  if (snake.hasGameEnded() == true) {
+    afterGame();              // Screen after the game that displays points and allows the user to restart
   }
   if ((snake.hasGameEnded() == false) && (snake.hasGameStarted() == true)) {
-    snake.drawSnake();
-    apple.drawApple();
-    if (frames % 15 == 0) {
+    snake.drawSnake();        // Draws the snake
+    apple.drawApple();        // Draws the apple
+    if (frames % 15 == 0) {   // Only moves the snake every 15 frames
       snake.moveSnake();
     }
-    if (apple.checkAppleCollision(snake) == true) {
-      apple.moveApple();
+    if (apple.checkAppleCollision() == true) {
+      points += 1;            // Add one to the user's score because the snake ate the apple
+      apple.moveApple();      // Move the apple to a new location
     }
-
   }
   window.requestAnimationFrame(main);
 }
@@ -25,12 +36,26 @@ function main() {
 // Set up the canvas and context objects
 context = setUpContext();
 
-// Create instance of Snake object
-let snake = new Snake ([ {x: 200, y: 320}, {x: 160, y: 320}, {x: 120, y: 320} ]);
-let apple = new Apple (480, 320);
+// Set the game board width as 600 x 600
+var gameBoardWidth = canvas.width - 50;
+var gameBoardHeight = canvas.height - 100;
 
+// All elements on the game board will be shifted because the board in centered on the canvas
+var xShift = 25
+var yShift = 75
+
+// Create instance of Snake object
+let snake = new Snake ([ {x: 200 + xShift, y: 320 + yShift}, {x: 160 + xShift, y: 320 + yShift}, {x: 120 + xShift, y: 320 + yShift} ]);
+let apple = new Apple (480 + xShift, 320 + yShift);
+
+// Set frames variable that will be used to only draw on certain frames
 var frames = 0;
 
+// Set user's points
+var points = 0;
+
+// Set up event listener for when user presses a key down.
+// It then calls the function snake.changeDirection, passing it an event object.
 document.addEventListener("keydown", snake.changeDirection);
 
 // Fire up the animation engine
